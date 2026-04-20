fa5909a8ccb8ecb852c13fbec0f88e4587233944
user <user@mehabooba>
2026-04-20 13:42:04 +0530

    mehaboob second commit

---
+++ com.crm.ClientManagementSystem/src/test/java/com/crm/ClientManagementSystem/Organization/Clickproduct.java
@@ -0,0 +1,13 @@
+package com.crm.ClientManagementSystem.Organization;
+
+import org.testng.annotations.Test;
+
+import com.crm.ClientManagementSystem.GenericLibrary.Baseclass;
+
+public class Clickproduct extends Baseclass {
+	@Test
+	public void product() {
+		
+	}
+
+}
